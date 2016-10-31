8e6a4a273c0a93a494240e7f7074e47dfbe2fc97
manuel <mfhenao88@gmail.com>
2016-10-31 00:09:53 -0500

    qwdfqwefqwef

--- app/src/main/java/co/com/apuestasmas/view/LoginActivity.java
+++ app/src/main/java/co/com/apuestasmas/view/LoginActivity.java
@@ -1,4 +1,4 @@
-package co.com.apuestasmas;
+package co.com.apuestasmas.view;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -31,6 +31,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import co.com.apuestasmas.R;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
