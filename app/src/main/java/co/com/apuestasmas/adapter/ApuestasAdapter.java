1b419a8537e3a3f6217fa2b42d9f9d9212d5d0a4
manuel <mfhenao88@gmail.com>
2016-11-01 07:54:01 -0500

    paquetes nuevos

---
+++ app/src/main/java/co/com/apuestasmas/adapter/ApuestasAdapter.java
@@ -0,0 +1,32 @@
+package co.com.apuestasmas.adapter;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by manuelhenao on 31/10/16.
+ */
+
+public class ApuestasAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
---
+++ app/src/main/java/co/com/apuestasmas/view/ApuestasActivity.java
@@ -0,0 +1,11 @@
+package co.com.apuestasmas.view;
+
+import android.app.ListActivity;
+
+/**
+ * Created by manuelhenao on 31/10/16.
+ */
+
+public class ApuestasActivity extends ListActivity {
+
+}
